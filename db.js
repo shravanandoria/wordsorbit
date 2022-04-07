0689889a9fdfd92cbcbf6932c231873fc9c0d46a
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2022-04-07 14:46:20 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const url = process.env.MONGO_URI;
-// "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
-// "mongodb+srv://wordsorbit:wordsorbit@wordsorbit.lsglg.mongodb.net/wordsorbit?retryWrites=true&w=majority";
 const connecToMongo = () =>
   mongoose
     .connect(url)
@@ -14,4 +12,3 @@ const connecToMongo = () =>
 
 module.exports = connecToMongo;
 
-// mongodb://localhost:27017/WordsOrbit?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false
